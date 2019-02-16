7e1002313bcf8926145cece04900e5bf2f384e28
riZ <50037152@europeia.pt>
2019-02-16 01:04:33 +0000

    added the basics

--- Unity Riot API/Assets/Scripts/API.cs
+++ Unity Riot API/Assets/Scripts/API.cs
@@ -1,10 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class API : MonoBehaviour
 {
 
     private const string URL = "www.google.com";
+    public Text responseText;
 
+    public void Request()
+    {
+        WWW request = new WWW(URL);
+        StartCoroutine(OnResponse(request));
+    }
+
+    private IEnumerator OnResponse(WWW req)
+    {
+        yield return req;
+
+        responseText.text = req.text;
+    }
 }
